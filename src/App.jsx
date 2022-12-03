5dbdcce61b751b996698e9d09a1b07d5ff43c654
MaximilianoZuniga50234 <maximiliano.zuniga@alumnos.frm.utn.edu.ar>
2022-12-02 23:11:33 -0300

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useSelector } from "react-redux";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+
 function App() {
-  return <div className="App"></div>;
+  const taskState = useSelector((state) => state.tasks);
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <TaskForm />
+      <TaskList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,7 @@
+import { useSelector } from "react-redux";
+
+const TaskForm = () => {
+  return <div>TaskForm</div>;
+};
+
+export default TaskForm;
